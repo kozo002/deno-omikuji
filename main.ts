72624205f5c44b35571493bb447bad3c9d2dd3ad
kozo yamagata <tune002@gmail.com>
2021-03-29 23:10:32 +0900

    Changes the content to be html

--- main.ts
+++ main.ts
@@ -7,8 +7,19 @@ const fortunes = [
 
 addEventListener('fetch', (event) => {
   const fortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-  const response = new Response(fortune, {
-    headers: { "content-type": "text/plain; charset=UTF-8" },
+  const html = `
+    <html>
+      <head>
+        <title>Omikuji</title>
+        <meta charset="utf-8" />
+      </head>
+      <body>
+        <h1>${fortune}</h1>
+      </body>
+    </html>
+  `
+  const response = new Response(html, {
+    headers: { "content-type": "text/html; charset=UTF-8" },
   });
   event.respondWith(response);
 })
